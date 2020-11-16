9a08465044b6e764c4ac481702ec0baf071736df
mohamed hawas <mohamedhawas123@gmail.com>
2020-11-16 22:54:19 +0200

    storage

--- src/index.js
+++ src/index.js
@@ -9,6 +9,21 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk'
 import userReducer from '../src/store/reducer/user'
 import dropdownReducer from '../src/store/reducer/dropdown'
+import { persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
+import {persistReducer} from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react';
+
+
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['dropdown']
+  
+}
+
+
 
 const composeEnhence = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
@@ -17,16 +32,28 @@ const rootReucer = combineReducers({
   dropdown: dropdownReducer
 })
 
-const store = createStore(rootReucer, composeEnhence(applyMiddleware(thunk)) )
+const pReducer = persistReducer(persistConfig,rootReucer )
+
+const store = createStore(pReducer, composeEnhence(applyMiddleware(thunk)) )
+
+export const persistor = persistStore(store);
+
+
 
 const app = (
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    
   </Provider>
 )
 
+
 ReactDOM.render(app, document.getElementById('root')
 );
+
+export default persistReducer(persistConfig, rootReucer);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
